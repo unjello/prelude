dddbe352ad6d91e6f6062b999019481adee7fa14
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2015-10-03 18:56:02 +0200

    [emacs] add org-mode inital config for tasks

---
+++ personal/01orgmode.el
@@ -0,0 +1,21 @@
+;; set agenda
+(setq org-agenda-skip-unavailable-files t)
+(setq org-agenda-files (list "~/Org/Personal"
+                             "~/Org/Personal/Pracownia"
+                             "~/Org/Work"))
+;; todo keywords
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
+
+(setq org-use-fast-todo-selection t)
+(setq org-treat-S-cursor-todo-selection-as-state-change nil)
+
+(setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("HOLD" ("WAITING") ("HOLD" . t))
+              (done ("WAITING") ("HOLD"))
+              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
